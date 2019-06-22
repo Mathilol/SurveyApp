4d188395eecf6510c1601391fb5645f00a71d742
geniusly <cedric.judicq@gmail.com>
2019-06-22 10:37:19 +0200

    added alot of stuff

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "524583938466-deju0omchnfg6cddb9irfump4geagbdf.apps.googleusercontent.com",
+  googleClientSecret: "UsVazVE523gOxw13NagvTwF0",
+  mongoURI:
+    "mongodb+srv://Mathilol:DGkVWKsy2fsxdqbO@emaily-ptsw3.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "dfisfdsfsmdfasdlfkdsfjieewp",
+  stripePublishableKey: "pk_test_YCklNpIMQ6G97la53WU6C4Eg00BFlvdnfP",
+  stripeSecretKey: "sk_test_btyEpm7ok0GkzCjs0GxbttAE002GTpY7Ai"
+};
